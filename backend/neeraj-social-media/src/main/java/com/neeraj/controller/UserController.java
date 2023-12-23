7ad34f29fe0216876fdd04492c0c47f63bbbd245
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-12-23 13:07:48 -0500

    feat: Enhance UserController and Add User model class
    1. Enhance getUsers endpoint in UserController:
    
       In this commit, I have made improvements to the getUsers endpoint in the UserController class:
    
       - Modified the return type to List<User> to provide a list of users.
       - Created and populated a list of User objects with sample data.
       - Added two User objects (user1 and user2) to the list.
    
       Now, when the "/users" endpoint is accessed, it returns a list containing user information, enhancing the functionality of the UserController.
    
    2. Add User model class:
    
       Additionally, I have introduced a new model class, User, to represent user data in the application. The User class includes the following fields:
    
       - firstName: String
       - lastName: String
       - email: String
       - password: String
    
       Two constructors (including a default constructor and a parameterized constructor) and getter/setter methods are implemented to facilitate the manipulation of user data.
    
       The User model class will be utilized to structure and manage user information within the application.

--- backend/neeraj-social-media/src/main/java/com/neeraj/controller/UserController.java
+++ backend/neeraj-social-media/src/main/java/com/neeraj/controller/UserController.java
@@ -1,13 +1,24 @@
 package com.neeraj.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.neera.models.User;
 
 @RestController
 public class UserController {
 	
 	@GetMapping("/users")
-	public String getUsers() {
-		return "get all users";
+	public List<User> getUsers() {
+		List<User> users=new ArrayList<>();
+		User user1=new User("code","neeraj","neeraj@gmail.com","12345");
+		User user2=new User("code","Ajay","ajay@gmail.com","123abc");
+		
+		users.add(user1);
+		users.add(user2);
+		return users;
 	}
 }
