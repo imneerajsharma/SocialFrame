8f994de4dceb11e52066c62a97fa4b48292277af
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-12-23 23:39:26 -0500

    feat: Enhance User model class and UserController
    1. Enhance User model class with ID field:
    
       In this commit, I have made enhancements to the User model class to include an additional field:
    
       - id: Integer
    
       A new constructor that includes the ID parameter is added to facilitate the instantiation of User objects with an ID. Getter and setter methods for the new ID field are also implemented.
    
       This modification allows for better tracking and management of user data by incorporating an identifier for each user in the application.
    
    2. Enhance UserController with CRUD operations:
    
       Additionally, I have made significant updates to the UserController class, introducing CRUD (Create, Read, Update, Delete) operations for managing user data:
    
       - GET mapping for "/users":
         - Retrieves a list of users with sample data.
    
       - GET mapping for "/users/{userId}":
         - Retrieves a specific user by ID.
    
       - POST mapping for "/users":
         - Creates a new user based on the provided request body.
    
       - PUT mapping for "/users":
         - Updates an existing user's information based on the provided request body.
    
       - DELETE mapping for "/users/{userId}":
         - Deletes a user with the specified ID.
    
       These enhancements provide comprehensive functionality for handling user-related operations in the application, making the UserController more robust and versatile.

--- backend/neeraj-social-media/src/main/java/com/neera/models/User.java
+++ backend/neeraj-social-media/src/main/java/com/neera/models/User.java
@@ -3,7 +3,7 @@ package com.neera.models;
 
 
 public class User {
-
+	private Integer id;
 	private String firstName;
 	private String lastName;
 	private String email;
@@ -13,13 +13,30 @@ public class User {
 		
 	}
 
-	public User(String firstName, String lastName, String email, String password) {
+	
+
+	public User(Integer id, String firstName, String lastName, String email, String password) {
 		super();
+		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
 	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
 
 	public String getFirstName() {
 		return firstName;
--- backend/neeraj-social-media/src/main/java/com/neeraj/controller/UserController.java
+++ backend/neeraj-social-media/src/main/java/com/neeraj/controller/UserController.java
@@ -3,7 +3,12 @@ package com.neeraj.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.neera.models.User;
@@ -11,14 +16,77 @@ import com.neera.models.User;
 @RestController
 public class UserController {
 	
+	
 	@GetMapping("/users")
-	public List<User> getUsers() {
+	public List<User> getUserId() {
 		List<User> users=new ArrayList<>();
-		User user1=new User("code","neeraj","neeraj@gmail.com","12345");
-		User user2=new User("code","Ajay","ajay@gmail.com","123abc");
+		User user1=new User(01,"code","neeraj","neeraj@gmail.com","12345");
+		User user2=new User(02,"code","Ajay","ajay@gmail.com","123abc");
 		
 		users.add(user1);
 		users.add(user2);
 		return users;
 	}
+	
+	@GetMapping("/users/{usersId}")
+	public User getUsers(@PathVariable("usersId")Integer id) {
+		
+		User user1=new User(01,"code","neeraj","neeraj@gmail.com","12345666");
+				
+		user1.setId(id);
+		
+		
+		return user1;
+	}
+	
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user) {
+		
+		User newUser=new User();
+		newUser.setEmail(user.getEmail());
+		newUser.setFirstName(user.getFirstName());
+		newUser.setLastName(user.getLastName());
+		newUser.setPassword(user.getPassword());
+		newUser.setId(user.getId());
+		return newUser;
+	}
+	
+	@PutMapping("/users")
+	public User updateUser(@RequestBody User user) {
+		User user1=new User(01,"code","neeraj","neeraj@gmail.com","12345");
+		
+		if(user.getFirstName()!=null) {
+			user1.setFirstName(user.getFirstName());
+		}
+		
+		if(user.getLastName()!=null) {
+			user1.setLastName(user.getLastName());
+		}
+		
+		if(user.getEmail()!=null) {
+			user1.setEmail(user.getEmail());
+		}
+		
+		if(user.getPassword()!=null) {
+			user1.setPassword(user.getPassword());
+		}
+		
+		
+		return user1;
+	}
+	
+	@DeleteMapping("/users/{userId}")
+	public String deleteUser(@PathVariable Integer userId) {
+		
+		return "user deleted successfully with id " + userId;
+	}
+	
+	
+	
+	
+
+	
+	
+	
+	
 }
